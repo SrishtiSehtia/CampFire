b4070cafd2ba0d19bcf24daa542a12a25f4d5d56
Srishti Sehtia <srishti0123@gmail.com>
2017-08-17 15:17:13 +0000

    add commennts model and add assosiations between comments and campgrounds

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require("express"),
     Campground = require("./models/campground"),
     seedDB      = require("./seeds")
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/yelp_camp_v3", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
