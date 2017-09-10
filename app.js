87524f514edf6a66c47cd92361c9c5b5d7fe3ed0
Srishti Sehtia <srishti0123@gmail.com>
2017-09-10 01:01:25 +0000

    prevent users from adding comment unless logged in via isLoggedIn middleware

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
    //lookup campground using ID
    Campground.findById(req.params.id, function(err, campground){
        if(err){
