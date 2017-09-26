1c1ddfd3dc3e7f8cba0aca02c4493019d840b4d8
Srishti Sehtia <srishti0123@gmail.com>
2017-09-26 15:32:49 +0000

    prevent unauthenticated user from creating a campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,8 +19,11 @@ router.post("/", isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
+    var desc = req.body.description;    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author:author}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
