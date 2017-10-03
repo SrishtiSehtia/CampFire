3a5a4db395f77ec4d08ec5dc270aa0c27dc1483c
Srishti Sehtia <srishti0123@gmail.com>
2017-10-03 15:17:40 +0000

    add edit form for campgrounds

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v8");
+mongoose.connect("mongodb://localhost/yelp_camp_v10");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
