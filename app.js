5b770f1d5ed869344993908c9c8146ecd777a5f5
Srishti Sehtia <srishti0123@gmail.com>
2017-10-21 20:26:01 +0000

    add mongolab

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://srishti:campfire@ds227565.mlab.com:27565/campfire1234");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
